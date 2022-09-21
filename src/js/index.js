e2ee13850fcde66ef959c508046c00b9ae99fa27
RoJaebl <qgs0924@gmail.com>
2022-09-21 14:43:13 +0900

    :hammer: Merge 'heap' file with 'main'

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,8 @@
-const sayHi = () => console.log("Hi");
+import { Heap } from "./heap";
 
-sayHi();
+const heap = new Heap();
+console.log(heap.heap, `len : ${heap.size()} `);
+[1, 4, 3, 5, 2, 6, 12, 11, 8, 7, 9, 10].forEach((value) => heap.insert(value));
+//[6, 4, 5, 1, 2].forEach((value) => heap.insert(value));
+console.log(heap.heap, `len : ${heap.size()} `);
+console.log(`delete : ${heap.delete()}`, heap.heap, `len : ${heap.size()} `);
